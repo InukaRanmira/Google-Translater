b06311dc474ee83a9b747ec021e688ccd54f48c2
Tinura Dinith <87059430+TinuraD@users.noreply.github.com>
2021-12-08 12:53:25 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -4,8 +4,7 @@ from helper.database import getid
 ADMIN = int(os.environ.get("ADMIN", 1696230986))
 
 @Client.on_message(filters.command("stats") & filters.user(ADMIN))
-async def broadcast(bot, message):
- if (message.reply_to_message):
+async def broadcast(_, message):
    users = len(getid())
    await message.reply_text(f"Total User - {users}")
 
